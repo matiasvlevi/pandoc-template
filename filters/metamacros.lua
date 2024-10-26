a55b7bbda2518bacdd8d6cf5063b065055fe6e71
matiasvlevi <matiasvlevi@gmail.com>
2024-10-26 02:53:14 -0400

    fix lua eval

--- filters/metamacros.lua
+++ filters/metamacros.lua
@@ -19,37 +19,77 @@ function process_macros(meta)
 
 	return macros
 end
+
+function fmt(str)
+	-- List of characters that need to be escaped in Lua patterns
+	local specialCharacters = "%(%)%.%[%]%*%+%-%?%^%$"
+
+	-- Use `gsub` to escape each occurrence of the special characters
+	return (str:gsub("([" .. specialCharacters .. "])", "%%%1"))
+end
+
+function igmatch(s, pattern)
+    local function iterator(s, index)
+        -- Use string.find with the pattern to locate matches
+        local startIndex, endIndex, content = string.find(s, pattern, index)
+        if content then
+            -- Return the content, start, and end index of the match
+            return endIndex + 1, content, startIndex, endIndex
+        end
+    end
+    
+    -- Return the iterator function, initial state, and initial index
+    return iterator, s, 1
+end
   
+function convertToTypographic(text)
+    -- Replace straight single and double quotes with typographic quotes
+    text = text:gsub("'", "’") -- Replace single quotes with curly single quotes
+    text = text:gsub('"', '“') -- Replace double quotes with curly double quotes
+    return text
+end
+
+-- Function to convert typographic quotes to straight quotes
+function convertToStraight(text)
+	text = text:gsub("’", "'");
+	text = text:gsub("‘", "'");
+    text = text:gsub('“', '"') -- Replace curly double quotes with straight double quote
+	text = text:gsub('”', '"') -- Replace curly double quotes with straight double quote
+    return text
+end
+
 function replace_macros(macros, text)
-	for literal in text:gmatch("{{(.-)}}") do
+	local new_text = text;
+	for _, literal, start_index, end_index in igmatch(text, "{{(.-)}}") do
 		local value = '';
 		
 		if macros[literal] then
 			-- Get from 
 			value = macros[literal];
-		else 
+		else
 			-- Evaluate as lua
-			
-			value = load('return current_macros.' .. literal)();
-			print(literal, value);
+			fvalue = load('local function index(a)\n'.. 
+			'  return a.' .. convertToStraight(literal) .. ';\n'..
+			'end;\n\nreturn index;');
 
-			literal = literal:gsub("%[", "%%[");
-			literal = literal:gsub("%]", "%%]");
-			literal = literal:gsub("%.", "%%.");
-			literal = literal:gsub("%\"", "%%\"");
-			literal = literal:gsub("%<", "%%<");
-			literal = literal:gsub("%>", "%%>");
-
-			--literal = literal:gsub("%", "%%'");
+			if fvalue ~= nil then
+				value =  fvalue()(macros)
+				literal = fmt(literal);
+			end
 		end
-		
-		text = text:gsub('{{'.. literal ..'}}', pandoc.utils.stringify(value));
+
+		new_text = new_text:gsub(
+			fmt(text:sub(start_index, end_index)), 
+			pandoc.utils.stringify(value)
+		)
 	end
 
-	return text
+	return new_text;
 end
 
 function Pandoc(doc)
+
+
 	current_macros = doc.meta;
 	local macros = process_macros(doc.meta)
 
--- src/page.md
+++ src/page.md
@@ -3,8 +3,16 @@ title: Template Document
 date: 2024-03-10
 author: [yourname, contributorname]
 keywords: [science, tech, engineering, math]
+notes:
+    todo: 'TODO:'
+    wip: 'NOTE: Work in progress'
 ---
 
 # Document 
 
 made by {{author[1]}} and {{author[2]}} on {{date}}
+
+
+## Description
+
+{{notes.wip}}
